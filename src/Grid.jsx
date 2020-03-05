80450e7925e1321bd9512d78db60c9d26cee23a1
Gastón Caruso <gstn.caruso@gmail.com>
2020-03-05 08:30:50 -0300

    Render current Temperature (only Buenos Aires for now) + Fetch api key from .env file

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import DateTime from "./DateTime";
 import BackgroundImage from "./BackgroundImage";
+import Weather from "./Weather";
 
 export default class Grid extends React.Component {
   render() {
     return (
       <section className={"Grid"}>
         <BackgroundImage refreshInterval={this.props.backgroundRefreshInterval} />
-        <DateTime dateTimeWidgetRefreshInterval={this.props.dateTimeWidgetRefreshInterval} />
+        <section className={"Info"}>
+          <DateTime dateTimeWidgetRefreshInterval={this.props.dateTimeWidgetRefreshInterval} />
+          <Weather />
+        </section>
       </section>
     );
   }
---
+++ src/Weather.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default class Weather extends React.Component {
+  apiKey = process.env.REACT_APP_WAPI_KEY;
+
+  constructor(props) {
+    super(props);
+    this.state = {temperature: 0, iconUrl: ''}
+  }
+
+  componentDidMount() {
+    this.fetchTemperature()
+  }
+
+  humanize = (str) => {
+    return str
+      .replace(/^[\s_]+|[\s_]+$/g, '')
+      .replace(/[_\s]+/g, ' ')
+      .replace(/^[a-z]/, function (m) {
+        return m.toUpperCase();
+      });
+  };
+
+  fetchTemperature = () => {
+    const currentCityName = "Buenos Aires";
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&appid=${this.apiKey}&units=metric`;
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+      const response = JSON.parse(xhr.responseText);
+
+      const temperature = response.main.feels_like;
+      const iconUrl = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+      this.setState({
+        temperature: temperature,
+        iconUrl: iconUrl
+      })
+    });
+
+    xhr.open('GET', apiUrl);
+    xhr.send();
+  };
+
+  render() {
+    return this.apiKey ?
+      <section className={"Weather"}>
+        <section className={"temperature"}>
+          <img src={this.state.iconUrl}/>
+          <p>{this.state.temperature}°C</p>
+        </section>
+      </section> : null
+  }
+}

fe351aeb5e58f948819a9168425a1632745625bc
Gastón Caruso <gstn.caruso@gmail.com>
2020-03-05 07:12:39 -0300

    Remove dead dependencies + Move BackgroundImage behaviour inside it

--- src/BackgroundImage.jsx
+++ src/BackgroundImage.jsx
@@ -1,4 +1,35 @@
 import React from "react";
 
-const BackgroundImage = props => <img src={props.imageUrl} className={"BackgroundImage"}/>;
+class BackgroundImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {imageUrl: ''}
+  }
+
+  clientResolution = () => `${window.innerWidth}x${window.innerHeight}`;
+
+  apiPath = () => `https://source.unsplash.com/random/${this.clientResolution()}`;
+
+  fetchImage = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+      const responseURL = xhr.responseURL;
+      this.setState({imageUrl: responseURL});
+    });
+
+    xhr.open('GET', this.apiPath());
+    xhr.send()
+  };
+
+  componentDidMount() {
+    this.fetchImage();
+    setInterval(this.fetchImage, this.props.refreshInterval);
+  }
+
+  render() {
+    return <img className={"BackgroundImage"} src={this.state.imageUrl}/>
+
+  }
+}
+
 export default BackgroundImage
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -3,35 +3,10 @@ import DateTime from "./DateTime";
 import BackgroundImage from "./BackgroundImage";
 
 export default class Grid extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { imageUrl: '' };
-  }
-
-  clientResolution = () => `${window.innerWidth}x${window.innerHeight}`;
-
-  apiPath = () => `https://source.unsplash.com/random/${this.clientResolution()}`;
-
-  fetchImage = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', () => {
-      const responseURL = xhr.responseURL;
-      this.setState({imageUrl: responseURL});
-    });
-
-    xhr.open('GET', this.apiPath());
-    xhr.send()
-  };
-
-  componentDidMount() {
-    this.fetchImage();
-    setInterval(this.fetchImage, this.props.backgroundRefreshInterval);
-  }
-
   render() {
     return (
       <section className={"Grid"}>
-        <BackgroundImage imageUrl={this.state.imageUrl} />
+        <BackgroundImage refreshInterval={this.props.backgroundRefreshInterval} />
         <DateTime dateTimeWidgetRefreshInterval={this.props.dateTimeWidgetRefreshInterval} />
       </section>
     );

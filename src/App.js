09ed0e5b546c3acc743962c60038579cdaf2b70a
Gastón Caruso <gstn.caruso@gmail.com>
2020-10-06 21:21:52 -0300

    Reduce background refresh interval

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Weather from "./Weather";
 import DateTime from "./DateTime";
 
 const oneSecond = 1000;
-const backgroundRefreshInterval = 10 * oneSecond;
+const backgroundRefreshInterval = 30 * oneSecond;
 const dateTimeWidgetRefreshInterval = oneSecond;
 const weatherRefreshInterval = 7200 * oneSecond;
 
 const App = () => {
-    return <div className={"Grid"}>
-      <div className={"Shadow"}/>
-      <BackgroundImage refreshInterval={backgroundRefreshInterval}/>
-      <div className={"Info"}>
-        <Weather refreshInterval={weatherRefreshInterval}/>
-        <DateTime dateTimeWidgetRefreshInterval={dateTimeWidgetRefreshInterval}/>
-      </div>
-    </div>;
-  }
-;
+  return <div className={"Grid"}>
+    <div className={"Shadow"} />
+    <BackgroundImage refreshInterval={backgroundRefreshInterval} />
+    <div className={"Info"}>
+      <Weather refreshInterval={weatherRefreshInterval} />
+      <DateTime dateTimeWidgetRefreshInterval={dateTimeWidgetRefreshInterval} />
+    </div>
+  </div>;
+}
+  ;
 
 export default App;
